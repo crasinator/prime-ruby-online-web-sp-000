fdfab945dffe1f89d90c8de70c0d74ff2de6f269
Jordan Reed <jordanreed03@gmail.com>
2020-12-06 01:39:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(num)
     if num % -1 == 0
       return false
     elsif num % 2 == 0 
-      return false
+      return true
     elsif num % 3 != 0
       return true
     elsif num % 5 != 0 
